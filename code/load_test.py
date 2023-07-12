20b9da2157452e56e77c26643e1c9c9439065ba0
13055 <1305567349@qq.com>
2023-07-12 10:08:16 +0800

    load_test.py用于测试存放人脸识别数据

---
+++ code/load_test.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+# 指定要打开的.npy文件的路径
+file_path = "E:\Python datas\SAFEFAACE_SUDA\code\model_data\mobilenet_face_encoding.npy"
+
+# load函数
+data = np.load(file_path)
+
+# 打印数据
+print(data[0][127])
