d81c38cec1adc556badf89c70d2ec062a39dae2d
13055 <1305567349@qq.com>
2023-07-13 14:27:13 +0800

    对database_connect中的putDatabase进行修改

--- code/database_connect.py
+++ code/database_connect.py
@@ -6,7 +6,7 @@ import numpy as np
 #       把东西放到数据库中
 # --------------------------------------------------------#
 # 连接到 SQL Server 数据库
-def putDatabase(name, known_face_encodings):
+def putDatabase(name, face_encoding):
     flat = 0
     conn = pyodbc.connect('DRIVER={SQL Server};SERVER=LEGION-Y9000P\SQLEXPRESS;DATABASE=SafeFace;UID=sa;PWD=')
 
@@ -14,7 +14,7 @@ def putDatabase(name, known_face_encodings):
     cursor = conn.cursor()
     try:
         # 将数据插入到数据库
-        character_info_np = np.array(known_face_encodings, dtype=np.float64)
+        character_info_np = np.array(face_encoding, dtype=np.float64)
         character_info_bytes = character_info_np.tobytes()
 
         # 将数据插入到数据库
@@ -77,24 +77,52 @@ def load_data():
     cursor = conn.cursor()
     cursor.execute("SELECT name, info from characters")
 
-    # 获取查询结果
     rows = cursor.fetchall()
-
-    # 结果保存在数组中
-    face_names = []
-    face_encodings = []
+    # 读取数据并存储到数组中
+    known_face_names = []
+    known_face_encodings = []
 
     for row in rows:
         name = row.name
-        info_bytes = row.info
-        matrix = np.frombuffer(info_bytes, dtype=np.float64)
-        face_names.append(name)
-        face_encodings.append(matrix)
+        info_byte = row.info
+        info = np.frombuffer(info_byte, dtype=np.float64)
 
-    return [face_names, face_encodings]
+        known_face_names.append(name)
+        known_face_encodings.append(info)
 
+    # 获取确切的数组长度
+    face_encoding_length = known_face_encodings[0].shape[0]
 
+    # 创建一个空的外部数组
+    num_faces = len(known_face_encodings)
+    known_face_encodings_padded = np.zeros((num_faces, face_encoding_length))
+
+    # 填充数组
+    for i, arr in enumerate(known_face_encodings):
+        known_face_encodings_padded[i, :] = arr
+
+    # 将列表转换为 NumPy 数组
+    known_face_names = np.array(known_face_names)
+    known_face_encodings = np.array(known_face_encodings_padded)
+
+    return [known_face_names, known_face_encodings]
+#     # 结果保存
+#     face_names = []
+#     face_encodings = []
+#
+#     for row in cursor:
+#         name = row.name
+#         info_bytes = row.info
+#         matrix = np.frombuffer(info_bytes, dtype=np.float64)
+#         face_names.append(name)
+#         face_encodings.append(matrix)
+#         face_names = np.array(face_names)
+#
+#     return [face_names, face_encodings]
+#
 if __name__ == '__main__':
     lst = load_data()
+    print(lst[0])
     for i in range(len(lst[0])):
-        print(lst[0][i], lst[1][i])
+        print(lst[1][i])
+
