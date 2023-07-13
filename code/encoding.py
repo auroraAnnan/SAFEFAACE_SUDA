816eaa7dd49454a837d24c7232eb0433d218737b
13055 <1305567349@qq.com>
2023-07-13 14:28:51 +0800

    encoding中增加函数putAll

--- code/encoding.py
+++ code/encoding.py
@@ -1,17 +1,16 @@
 import os
-
 from retinaface import Retinaface
+def putAll(dir):
+    '''
+    在更换facenet网络后一定要重新进行人脸编码，运行encoding.py。
+    '''
+    retinaface = Retinaface(1)
 
-'''
-在更换facenet网络后一定要重新进行人脸编码，运行encoding.py。
-'''
-retinaface = Retinaface(1)
+    list_dir = os.listdir(dir)
+    image_paths = []
+    names = []
+    for name in list_dir:
+        image_paths.append(dir + "/" + name)
+        names.append(name.split(".")[0])
 
-list_dir = os.listdir("face_dataset")
-image_paths = []
-names = []
-for name in list_dir:
-    image_paths.append("face_dataset/"+name)
-    names.append(name.split("_")[0])
-
-retinaface.encode_face_dataset(image_paths, names)
+    retinaface.encode_face_dataset(image_paths, names)
