ca292ad4c0734512017f02bb1f9255d790d7d93c
13055 <1305567349@qq.com>
2023-07-14 08:57:02 +0800

    修正文件路径格式

--- code/load_test.py
+++ code/load_test.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 # 指定要打开的.npy文件的路径
-file_path = "E:\Python datas\SAFEFAACE_SUDA\code\model_data\mobilenet_names.npy"
+file_path = r"E:\Python datas\SAFEFAACE_SUDA\code\model_data\mobilenet_names.npy"
 
 # load函数
 data = np.load(file_path)
