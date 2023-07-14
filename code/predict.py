ad97fcb8c4fe4a3b99a69405be48ae2502762cad
13055 <1305567349@qq.com>
2023-07-14 08:54:54 +0800

    部分代码规范更改

--- code/predict.py
+++ code/predict.py
@@ -8,29 +8,28 @@ from threading import Event
 import cv2
 import numpy as np
 
-
 from retinaface import Retinaface
 from utils.video import CameraThread, FileVideoStream
 
 if __name__ == "__main__":
     retinaface = Retinaface()
-    # ----------------------------------------------------------------------------------------------------------#
+    # ---------------------------------------------------------------------------------------------#
     #   mode用于指定测试的模式：
-    #   'predict'表示单张图片预测，如果想对预测过程进行修改，如保存图片，截取对象等，可以先看下方详细的注释
-    #   'video'表示视频检测，可调用摄像头或者视频进行检测，详情查看下方注释。
-    #   'fps'表示测试fps，使用的图片是img里面的street.jpg，详情查看下方注释。
-    #   'dir_predict'表示遍历文件夹进行检测并保存。默认遍历img文件夹，保存img_out文件夹，详情查看下方注释。
-    # ----------------------------------------------------------------------------------------------------------#
+    #   predict表示单张图片预测，如果想对预测过程进行修改，如保存图片，截取对象等，可以先看下方详细的注释
+    #   video表示视频检测，可调用摄像头或者视频进行检测，详情查看下方注释。
+    #   fps表示测试fps，使用的图片是img里面的street.jpg，详情查看下方注释。
+    #   dir_predict表示遍历文件夹进行检测并保存。默认遍历img文件夹，保存img_out文件夹，详情查看下方注释。
+    # ---------------------------------------------------------------------------------------------#
     mode = "video"
-    # ----------------------------------------------------------------------------------------------------------#
+    # ---------------------------------------------------------------------------------------------#
     #   video_path用于指定视频的路径，当video_path=0时表示检测摄像头
     #   想要检测视频，则设置如video_path = "xxx.mp4"即可，代表读取出根目录下的xxx.mp4文件。
     #   video_save_path表示视频保存的路径，当video_save_path=""时表示不保存
     #   想要保存视频，则设置如video_save_path = "yyy.mp4"即可，代表保存为根目录下的yyy.mp4文件。
     #   video_fps用于保存的视频的fps
-    #   video_path、video_save_path和video_fps仅在mode='video'时有效
+    #   video_path、video_save_path和video_fps仅在mode=video时有效
     #   保存视频时需要ctrl+c退出或者运行到最后一帧才会完成完整的保存步骤。
-    # ----------------------------------------------------------------------------------------------------------#
+    # ---------------------------------------------------------------------------------------------#
     video_path = 0
     video_save_path = ""
     video_fps = 25.0
@@ -75,12 +74,6 @@ if __name__ == "__main__":
                 cv2.waitKey(0)
 
     elif mode == "video":
-        # capture = cv2.VideoCapture(video_path)
-        #
-        # if video_save_path!="":
-        #     fourcc = cv2.VideoWriter_fourcc(*'XVID')
-        #     size = (int(capture.get(cv2.CAP_PROP_FRAME_WIDTH)), int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT)))
-        #     out = cv2.VideoWriter(video_save_path, fourcc, video_fps, size)
 
         # 提高fps 开始
         if video_path == 0:
@@ -103,17 +96,16 @@ if __name__ == "__main__":
         (width, length, depth) = frame.shape
 
         fps = 0.0
-        while (True):
+        while True:
             # image_cropper = CropImage()
             t1 = time.time()
             # 读取某一帧;
             ref, frame = capture.read()
-            frame = frame[int(width*0.2):int(width*0.8), int(length*0.3):int(length*0.7), :]
+            frame = frame[int(width * 0.2):int(width * 0.8), int(length * 0.3):int(length * 0.7), :]
             if not ref:
                 break
             # 格式转变，BGRtoRGB
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
 
             # 进行检测
             frame = np.array(retinaface.detect_image(frame))
@@ -121,8 +113,9 @@ if __name__ == "__main__":
             frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
 
             fps = (fps + (1. / (time.time() - t1))) / 2
-            print("fps= %.2f" % (fps))
-            # frame = cv2.putText(frame, "fps= %.2f" % (fps), (0, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            print("fps= %.2f" % fps)
+            # frame = cv2.putText(frame, "fps= %.2f" % (fps), (0, 40),
+            # cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
             cv2.namedWindow("video", cv2.WINDOW_KEEPRATIO)
             cv2.imshow("video", frame)
             c = cv2.waitKey(1) & 0xff
@@ -152,7 +145,8 @@ if __name__ == "__main__":
         img_names = os.listdir(dir_origin_path)
         for img_name in tqdm(img_names):
             if img_name.lower().endswith(
-                    ('.bmp', '.dib', '.png', '.jpg', '.jpeg', '.pbm', '.pgm', '.ppm', '.tif', '.tiff')):
+                    ('.bmp', '.dib', '.png', '.jpg', '.jpeg',
+                     '.pbm', '.pgm', '.ppm', '.tif', '.tiff')):
                 image_path = os.path.join(dir_origin_path, img_name)
                 image = cv2.imread(image_path)
                 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -162,4 +156,5 @@ if __name__ == "__main__":
                     os.makedirs(dir_save_path)
                 cv2.imwrite(os.path.join(dir_save_path, img_name), r_image)
     else:
-        raise AssertionError("Please specify the correct mode: 'predict', 'video', 'fps' or 'dir_predict'.")
+        raise AssertionError("Please specify the correct mode: "
+                             "'predict', 'video', 'fps' or 'dir_predict'.")
