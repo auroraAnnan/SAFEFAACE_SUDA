1416b58e08ab3df7094e8fa760ba904827e656d5
13055 <1305567349@qq.com>
2023-07-14 08:58:37 +0800

    调用interface中的database_interface接口

--- code/put_database.py
+++ code/put_database.py
@@ -1,18 +1,24 @@
 import pyodbc
 import numpy as np
+
+from interface import database_interface
+
+
 # --------------------------------------------------------#
 #       把东西放到数据库中
 # --------------------------------------------------------#
 # 连接到 SQL Server 数据库
-def putDatabase(name, known_face_encodings):
+def put_database(name, known_face_encodings):
     flat = 0
-    conn = pyodbc.connect('DRIVER={SQL Server};SERVER=.;DATABASE=users;UID=sa;PWD=1234')
+    # Server, DATABASE, UID, PWD需要自行修改
+    conn = pyodbc.connect(database_interface('{SQL Server}',
+                                             r'LEGION-Y9000P\SQLEXPRESS', 'SafeFace', 'sa', ''))
 
     # 创建一个游标对象，用于执行 SQL 语句
     cursor = conn.cursor()
     try:
         # 将数据插入到数据库
-        character_info_np = np.array(known_face_encodings)
+        character_info_np = np.array(known_face_encodings, dtype=np.float64)
         character_info_bytes = character_info_np.tobytes()
 
         # 将数据插入到数据库
